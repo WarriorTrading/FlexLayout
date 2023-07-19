6e23bf42269cdbe4451ab425af0d157c23ddcf06
BenjaminLiuNirvana <benjamin.liu@nirvana-info.com>
2023-07-20 02:05:00 +0800

    test

--- src/model/Utils.ts
+++ src/model/Utils.ts
@@ -2,8 +2,8 @@ import { TabSetNode } from "./TabSetNode";
 import { BorderNode } from "./BorderNode";
 import { RowNode } from "./RowNode";
 import { TabNode } from "./TabNode";
-// import { SHA256 } from "crypto-js";
-// import { v4 as getUUID } from "uuid";
+import { SHA256 } from "crypto-js";
+import { v4 as getUUID } from "uuid";
 
 /** @internal */
 export function adjustSelectedIndexAfterFloat(node: TabNode) {
@@ -70,14 +70,14 @@ export function adjustSelectedIndex(parent: TabSetNode | BorderNode | RowNode, r
     }
 }
 
-// // let id = 0
+let id = 0
 
-// /** @internal */
-// export function getGeneratedSeedId() {
-//     if(window.name != null) {
-//         window.name = getUUID();
-//     }
-//     const newSeedId =  SHA256(window.name + id);
-//     id++;
-//     return newSeedId;
-// }
+/** @internal */
+export function getGeneratedSeedId() {
+    if(window.name != null) {
+        window.name = getUUID();
+    }
+    const newSeedId =  SHA256(window.name + id);
+    id++;
+    return newSeedId;
+}
